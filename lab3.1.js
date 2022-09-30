599ae2428f5b95a45119b67ae5f82184667b80d9
Vitaliy <legetavitalik@gmail.com>
2022-09-30 23:16:42 +0300

    lab3

---
+++ lab3.1.js
@@ -0,0 +1,10 @@
+let desk = ' # # # # # # # #'
+let desk1 = '# # # # # # # # '
+console.log(desk)
+console.log(desk1)
+console.log(desk)
+console.log(desk1)
+console.log(desk)
+console.log(desk1)
+console.log(desk)
+console.log(desk1)
---
+++ lab3.2.js
@@ -0,0 +1,7 @@
+for (let number = 1; number < 101; number++) {
+    if (number % 3 === 0) {
+      console.log('welcome to hell');
+    } else {
+      console.log(number);
+    }
+  }
